ed328266621e29e38103488e344083d54ce15669
krishnaSarkar5 <krishnasarkar95@gmail.com>
2022-10-19 08:45:47 +0530

    implementinf=g quiz ui

--- src/App.js
+++ src/App.js
@@ -11,17 +11,28 @@ import axios from 'axios';
 
 function App() {
 const [name, setName] = useState("");
-const [questions, setQuestions] = useState();
+const [questions, setQuestions] = useState([]);
 const [score, setScore] = useState(0);
 
 
-  const fetchQuestions = async (category="",difficulty="")=>{
-    const {data} = await axios.get(
-      `https://opentdb.com/api.php?amount=10${category && `&category=${category}`}${difficulty && `&difficulty=${difficulty}`}&type=multiple`
-    )
-    console.log(data);
-  };
+  const fetchQuestions =  (category="",difficulty="")=>{
+    // const {data} = await axios.get(
+    //   `https://opentdb.com/api.php?amount=10${category && `&category=${category}`}${difficulty && `&difficulty=${difficulty}`}&type=multiple`
+    // )
 
+
+      axios.get(`https://opentdb.com/api.php?amount=10${category && `&category=${category}`}${difficulty && `&difficulty=${difficulty}`}&type=multiple`)
+      .then((response)=>{
+        setQuestions(response.data.results)
+      })
+
+
+    // console.log(data.results);
+    // setQuestions(data.results);
+    // console.log(questions);
+  };
+  let allQuestions = questions;
+  console.log(allQuestions);
   return (
     <BrowserRouter>
      <div className='app' style={{backgroundImage:"url(./backgroundImage.png)"}}>
@@ -30,7 +41,12 @@ const [score, setScore] = useState(0);
           <Route path='/' element={<Home name={name}
                                          setName={setName}
                                          fetchQuestions={fetchQuestions} />}/>
-          <Route path='/quiz' element={<Quiz/>}/>
+          <Route path='/quiz' element={<Quiz 
+                                             name={name} 
+                                             questions={questions}
+                                             score={score}
+                                             setScore={setScore}
+                                             setQuestions={setQuestions}/>}/>
           <Route path='/result' element={<Result/>}/>
         </Routes>
       </div>
--- src/Pages/Quiz/Quiz.js
+++ src/Pages/Quiz/Quiz.js
@@ -1,8 +1,41 @@
-import React from 'react'
+import React, { useEffect,useState } from 'react'
+import { CircularProgress } from '@material-ui/core';
 
-const Quiz = () => {
+const Quiz = ({name,questions,score,setScore,setQuestions}) => {
+
+  const [options, setOptions] = useState();
+  const [currQues, setCurrQues] = useState(0);
+
+  const handleShuffle = (opt)=>{
+    return opt.sort(()=> Math.random()-0.5);
+  }
+console.log("options ",options);
+
+  useEffect(()=>{
+    console.log("+++ inside useeffect quiz.js : ",questions);
+  //   console.log(questions[currQues]?.correct_answer)
+  //  const a= questions[currQues]?.incorrect_answers
+  //   console.log(a)
+  //   console.log([questions[currQues]?.correct_answer].concat(questions[currQues]?.incorrect_answers))
+  //   const arr= handleShuffle([
+  //                       questions[currQues]?.correct_answer,
+  //                       a,
+  //                   ]) 
+    setOptions(
+      (questions && handleShuffle([questions[currQues]?.correct_answer].concat(questions[currQues]?.incorrect_answers)) )
+    )
+  },[questions]);
+
   return (
-    <div>Quiz</div>
+    <div>
+      <span className='subtitle'> Welcome,{name} </span>
+      {questions ? (<></>):(
+        <CircularProgress style={{margin:"auto",display:"flex",justifyContent:"center"}}
+                          color='inherit'
+                          size={150}
+                          thickness={1}/>
+      )}
+    </div>
   )
 }
 
